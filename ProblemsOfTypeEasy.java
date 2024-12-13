d2d0f11e81703fb6a7a9f48f356e92b4d1a326a7
mallikarjun meti <89732992+mgmeti@users.noreply.github.com>
2024-12-13 13:20:52 +0530

    Finding the number of occurence of target in given sorted array
    Binary search strategy is used to tackle the problem

--- ProblemsOfTypeEasy.java
+++ ProblemsOfTypeEasy.java
@@ -7,6 +7,30 @@ public class ProblemsOfTypeEasy {
         return (s1+s1).lastIndexOf(s2) >= 0;      
     }
 
+   // Given a sorted array, arr[] and a number target, you need to find the number of occurrences of target in arr[]. 
+  public static int countFreq(int[] arr, int target) { 
+        int low = 0;
+        int high = arr.length - 1;
+        int freq = 0;
+      
+        while(low <= high){
+            int mid =low + (high - low) / 2;            
+            if(target == arr[mid] ){
+                for(int i = low; i <= high; i++){
+                    if(arr[i] == target) freq++;  
+                }           
+                break;
+            }            
+            if( target > arr[mid]){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }        
+        return freq;
+    }
+    
+
 
   
 }
